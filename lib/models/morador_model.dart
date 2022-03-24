a201d99448a209421c0ddbcd98376063867ce3c9
Leuow95 <leuow95@gmail.com>
2022-03-23 22:51:13 -0400

    :art: implemented the fromJson in the Model

--- lib/models/morador_model.dart
+++ lib/models/morador_model.dart
@@ -4,4 +4,12 @@ class MoradorModel {
   final int apt;
 
   MoradorModel({required this.name, required this.bloco, required this.apt});
+
+  factory MoradorModel.fromJson(Map<String, dynamic> json) {
+    return MoradorModel(
+      name: json["name"],
+      bloco: json["bloco"],
+      apt: json["apt"],
+    );
+  }
 }
