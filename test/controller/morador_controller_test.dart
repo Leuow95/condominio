4ad152432ddf2343270ccd56a6706a2bf6550fd4
Leuow95 <leuow95@gmail.com>
2022-03-23 22:52:40 -0400

    :white_check_mark: implemented the tests

---
+++ test/controller/morador_controller_test.dart
@@ -0,0 +1,44 @@
+import 'package:condominio/controller/morador_controller.dart';
+import 'package:condominio/models/morador_model.dart';
+import 'package:condominio/services/morador_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MoradorServiceMock extends Mock implements MoradorService {}
+
+class MoradorControllerMock extends Mock implements MoradorController {}
+
+void main() {
+  final service = MoradorServiceMock();
+
+  final controller = MoradorController(service);
+  test("Deve preencher variavel moradores", () async {
+    when(() => service.getMoradores()).thenAnswer(
+      (_) async => Future.delayed(
+        const Duration(milliseconds: 800),
+        () => [
+          MoradorModel(
+            name: "Leonardo",
+            apt: 01,
+            bloco: 01,
+          )
+        ],
+      ),
+    );
+
+    await controller.start();
+
+    // expect(controller.moradores[0].name, "Leonardo");
+    expect(controller.moradores, isA<List<MoradorModel>>());
+  });
+
+  test("Deve preencher variavel moradores", () async {
+    when(() => service.getMoradores())
+        .thenAnswer((_) async => <MoradorModel>[]);
+
+    await controller.start();
+
+    // expect(controller.moradores[0].name, "Leonardo");
+    expect(controller.moradores, isA<List<MoradorModel>>());
+  });
+}
